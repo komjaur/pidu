d0535345c856bdf3b2744668e8e1370f94e57925
komjaur <komjaur@gmail.com>
2017-02-08 10:34:14 +0200

    sdxfsf
    sfdsdf

---
+++ Pidu/Pidu/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Pidu
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        korter korter1 = new korter();
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private void lisaButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (!string.IsNullOrWhiteSpace(textbox.Text))
+            {
+      
+                korter1.lisaInimene(textblock1.Text);
+                textbox1.Text = "";
+                textblock1.Text = korter1.kuvaInimesed();
+            }else
+            {
+
+                var dialog = new MessageDialog("all full!!!!!");
+                dialog.ShowAsync();
+            }
+            
+        }
+
+        private void eemaldaButton_Click(object sender, RoutedEventArgs e)
+        {
+            korter1.eemaldaInimene(textblock1.Text);
+            textbox1.Text = "";
+            textblock1.Text = korter1.kuvaInimesed();
+        }
+    }
+}
